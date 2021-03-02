d62831e91b22a41bbf3d7ed74d03bb2885406253
Alexander Fengler <alexanderfengler@gmx.de>
2021-03-02 13:28:35 -0500

    ..

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-        ext_modules = cythonize("cddm_data_simulation.pyx", 
+        ext_modules = cythonize("simulators/cddm_data_simulation.pyx", 
                                 annotate = True, 
                                 compiler_directives = {"language_level": "3"}),
         include_dirs = [numpy.get_include()]
