ea427fc67ab61650cea06f226a61cc23fdd0bc37
Alexander Fengler <alexanderfengler@gmx.de>
2021-07-29 00:38:00 -0400

    delete some unnecessary files

--- data_storage/hddm-nn_likelihood/hddm/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-import unittest
-
-from .test_models import *
-from .test_likelihoods import *
--- unused/data_storage/hddm-nn_likelihood/clean_build.sh
+++
@@ -1,3 +0,0 @@
-rm src/*.c *.so -rf build
-git checkout src/cdfdif.c
-python setup.py build_ext --inplace
